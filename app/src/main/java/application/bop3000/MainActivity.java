a2410a06c57800f922923b7f5263462f82ce7766
Bjørnar <os.bjornar@gmail.com>
2021-01-20 11:39:16 +0100

    menu

--- app/src/main/java/application/bop3000/MainActivity.java
+++ app/src/main/java/application/bop3000/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import application.bop3000.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
